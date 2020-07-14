1a93e82b93e247d18cc948f276a9ed4869c98915
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-14 15:54:42 +0800

    change list to render random lucky number

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const name = "firdaus"
+const luckyNumber = Math.floor(Math.random() * 10);
+
 ReactDOM.render(
   <div>
-    <h1>Hello world</h1>
-    <ul>
-      <li>nihao</li>
-      <li>salam</li>
-      <li>hej</li>
-    </ul>
+    <h1>Hello {name}</h1>
+    <p>You lucky number is {luckyNumber}</p>
+
   </div>,
   document.getElementById('root')
 );
