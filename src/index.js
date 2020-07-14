5c873ae7a5fe604f72db00b1dd25c13892b541bc
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-14 13:58:19 +0800

    add unordered list and wrapped all in one div

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1>Hello world</h1>,
+  <div>
+    <h1>Hello world</h1>
+    <ul>
+      <li>nihao</li>
+      <li>salam</li>
+      <li>hej</li>
+    </ul>
+  </div>,
   document.getElementById('root')
 );
