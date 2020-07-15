6ba344e53406272cd3cda954eb1b35938b2afe4d
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-15 23:56:12 +0800

    test with className and alt

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ ReactDOM.render(
     <p>Created by {fName + ' ' + lName}</p>
     <p>Copyright {newYear}</p>
     <h1>My favourite Drink</h1>
-    <img src='https://i.pinimg.com/originals/c6/3d/7e/c63d7ecf7c62fd4cdd079699cfb818de.jpg' />
-    <img src='https://www.ecomall.my/image/cache/data/product-32046/kopi%20ais-800x800.jpg' />
-    <img src='https://www.daily-groceries.com/wp-content/uploads/2019/09/nescafe-ais-1.jpg' />
+    <img className='food-img' alt='teh ais' src='https://i.pinimg.com/originals/c6/3d/7e/c63d7ecf7c62fd4cdd079699cfb818de.jpg' />
+    <img className='food-img' alt='kopi ais' src='https://www.ecomall.my/image/cache/data/product-32046/kopi%20ais-800x800.jpg' />
+    <img className='food-img' alt='nescafe ais' src='https://www.daily-groceries.com/wp-content/uploads/2019/09/nescafe-ais-1.jpg' />
   </div>,
   document.getElementById('root')
 );
