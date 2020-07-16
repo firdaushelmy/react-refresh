4fe663fce478175c14d859514fde4761935053c3
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-16 18:33:52 +0800

    refactor code to component +  add eslint airbnb style

---
+++ src/components/Heading.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Heading() {
+  const fName = 'firdaus';
+  const lName = 'helmy';
+  const currentHour = new Date().getHours();
+  let greetingMessage = '';
+  const headingColor = {
+    color: ""
+  };
+
+  if (currentHour < 12) {
+    greetingMessage = 'Good Morning';
+    headingColor.color = 'red';
+  } if (currentHour >= 12 && currentHour < 18) {
+    greetingMessage = 'Good Afternoon';
+    headingColor.color = 'green';
+  } if (currentHour >= 18 && currentHour < 24) {
+    greetingMessage = 'Good Afternoon';
+    headingColor.color = 'blue';
+  }
+  return (
+    <div>
+      <h1 className='heading' style={headingColor}>
+        {greetingMessage}
+        {` ${fName} ${lName} !`}
+      </h1>
+    </div>
+  )
+}
+
+export default Heading;
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Heading from './components/Heading';
+
+ReactDOM.render(
+  <Heading />
+  , document.getElementById('root')
+);
